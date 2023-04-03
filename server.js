15cd5f590355a4e00aa7d51f15f27b80f290d1ab
Kaeneth Dayao <kendayao@gmail.com>
2023-04-02 17:11:14 -0700

    displaymulitple servers

--- server.js
+++ server.js
@@ -6,15 +6,42 @@ var PORT = 8080;
 var server = http.createServer(handleRequest);
 
 
-function handleRequest(req, res){
-    fs.readFile(__dirname+"/index.html", function(err, data){
-        if (err) throw err;
+function handleRequest(req,res){
 
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(data)
-    })
+    var path = req.url;
+
+    switch (path){
+        case "/food":
+            return fs.readFile(__dirname + "/food.html", function(err,data){
+                if (err) throw err;
+                res.writeHead(200, {"Content-Type":"text/html"});
+                res.end(data)
+                console.log(data)
+            });
+
+        case "/movies":
+            return fs.readFile(__dirname + "/movies.html", function(err,data){
+                if (err) throw err;
+                res.writeHead(200, {"Content-Type":"text/html"});
+                res.end(data)
+                console.log(data)
+            })
+
+        default:
+            return fs.readFile(__dirname + "/index.html", function(err,data){
+                if (err) throw err;
+                res.writeHead(200, {"Content-Type":"text/html"});
+                res.end(data)
+                console.log(data)
+            })
+
+
+
+
+    }
+
+
 }
-
 
 
 
