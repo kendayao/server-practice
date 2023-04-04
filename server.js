03dec687b464535d0465f9cf4bd4edc5c251317c
Kaeneth Dayao <kendayao@gmail.com>
2023-04-03 21:27:04 -0700

    installed npm

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 var http = require("http");
 var fs = require("fs");
 
@@ -5,52 +6,61 @@ var PORT = 8080;
 
 var server = http.createServer(handleRequest);
 
+function handleRequest(req, res) {
+  var path = req.url;
 
-function handleRequest(req,res){
-
-    var path = req.url;
-
-    switch (path){
-        case "/food":
-            return fs.readFile(__dirname + "/food.html", function(err,data){
-                if (err) throw err;
-                res.writeHead(200, {"Content-Type":"text/html"});
-                res.end(data)
-                console.log(data)
-            });
-
-        case "/movies":
-            return fs.readFile(__dirname + "/movies.html", function(err,data){
-                if (err) throw err;
-                res.writeHead(200, {"Content-Type":"text/html"});
-                res.end(data)
-                console.log(data)
-            })
-
-        default:
-            return fs.readFile(__dirname + "/index.html", function(err,data){
-                if (err) throw err;
-                res.writeHead(200, {"Content-Type":"text/html"});
-                res.end(data)
-                console.log(data)
-            })
-
-
-
-
-    }
-
-
+  switch (path) {
+  case "/thanks":
+    return renderThankYouPage(req, res);
+  default:
+    return renderWelcomePage(req, res);
+  }
 }
 
+function renderWelcomePage(req, res) {
+  fs.readFile(__dirname + "/index.html", function(err, data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+    }
+    else {
+      // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+      // an html file.
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+}
+
+function renderThankYouPage(req, res) {
+  // Saving the request posted data as a variable.
+  var requestData = "";
+
+  var myHTML =
+    "<html><head><title>Hello Noder!</title></head><body><h1>Oops, I didn't get any data</h1></body></html>";
+
+  // When the server receives data, it will add it to requestData.
+  req.on("data", function(data) {
+    requestData += data;
+    console.log("You just posted some data to the server:\n", requestData);
+    console.log(data)
+    myHTML =
+      "<html><head><title>Hello Noder!</title></head><body>" +
+      "<h1>Thank you for the data: </h1><code>" +
+      requestData +
+      "</code>" +
+      "</body></html>";
+  });
 
 
+  // When the request has ended...
+  req.on("end", function() {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(myHTML);
+  });
+}
 
-// Start our server
+// Starts our server.
 server.listen(PORT, function() {
-  // Callback triggered when server is successfully listening. Hurray!
   console.log("Server listening on: http://localhost:" + PORT);
 });
-
-
-
