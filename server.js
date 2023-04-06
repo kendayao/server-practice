c4cb53aa23d6cb39e4052fb1fc2d63cfbc276d25
Kaeneth Dayao <kendayao@gmail.com>
2023-04-05 20:47:30 -0700

    data

--- server.js
+++ server.js
@@ -10,9 +10,21 @@ var yoda = {
   role: "Jedi Master",
   age: 900,
   forcePonts: 2000
-}
+};
 
+var dartmaul = {
+  name: "Darth Maul",
+  role: "Sith Lord",
+  age: 200,
+  forcePonts: 1200
+};
 
+var obiwankenobi = {
+  name: "Obi Wan Kenobi",
+  role: "Jedi",
+  age: 150,
+  forcePonts: 1500
+};
 
 
 
